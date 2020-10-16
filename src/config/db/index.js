a4e88da2a2c6205f5750366edeb39fab7fccb2f5
Silver Miu <giahunghust@gmail.com>
2020-10-16 11:39:11 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect() {
   try {
     await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
-useNewUrlParser: true,
+      useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
